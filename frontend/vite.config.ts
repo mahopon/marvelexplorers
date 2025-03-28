cec5cd24269b6de1d456b8269436e9b511cb4379
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-03-29 03:45:35 +0800

    Modified vite build settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name!.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType!)) {
+            extType = 'img';
+          }
+          return `marvelexplorers/assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'marvelexplorers/assets/js/[name]-[hash].js',
+        entryFileNames: 'marvelexplorers/assets/js/[name]-[hash].js',
+      }
+    }
+  }
 })
