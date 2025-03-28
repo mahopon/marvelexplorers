05cb79a2d3dd940ab628ad49b42c71a0790013f8
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-03-29 02:15:54 +0800

    Remade frontend folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import './styles/App.css';
-import Home from "./pages/Home.tsx"
+import './styles/App.css'
+import Home from './pages/Home'
 
 const App: React.FC = () => {
   return (
